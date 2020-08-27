644ce9b5cd1a58776c0f401ae7b9b269dad0d3c6
Krzysztof <crunkz1995@gmail.com>
2020-08-28 00:35:08 +0200

    Add app configuration file

---
+++ Sources/BeerPal/App/AppConfig.swift
@@ -0,0 +1,16 @@
+//
+//  AppConfig.swift
+//  BeerPal
+//
+//  Created by Krzysztof Babis on 27.08.2020 r..
+//  Copyright © 2020 Krzysztof Babis. All rights reserved.
+//
+
+import Foundation
+
+struct AppConfig {
+    enum API {
+        static let host: String = "sandbox-api.brewerydb.com"
+        static let version: String = "v2"
+    }
+}
