8e49d52648b8fe97dc444ead1608bf32cfb53525
Krzysztof <crunkz1995@gmail.com>
2020-08-28 00:35:59 +0200

    Add URLBuilder along with tests

---
+++ Sources/BeerPal/Services/Networking/URLBuilder.swift
@@ -0,0 +1,47 @@
+//
+//  URLBuilder.swift
+//  BeerPal
+//
+//  Created by Krzysztof Babis on 27.08.2020 r..
+//  Copyright © 2020 Krzysztof Babis. All rights reserved.
+//
+
+import Foundation
+
+final class URLBuilder {
+    private var components: URLComponents
+    private let version: String?
+
+    init(scheme: String = "https", host: String = AppConfig.API.host, version: String? = AppConfig.API.version) {
+        self.components = URLComponents()
+        self.components.scheme = scheme
+        self.components.host = host
+        self.version = version
+    }
+
+    func set(port: Int) -> URLBuilder {
+        components.port = port
+        return self
+    }
+
+    func set(path: String) -> URLBuilder {
+        var path = version ?? "" + path
+        if !path.hasPrefix("/") {
+            path = "/" + path
+        }
+        components.path = path
+        return self
+    }
+
+    func addQueryItem(name: String, value: String) -> URLBuilder {
+        if components.queryItems == nil {
+            components.queryItems = []
+        }
+        components.queryItems?.append(URLQueryItem(name: name, value: value))
+        return self
+    }
+
+    func build() -> URL? {
+        return components.url
+    }
+}
