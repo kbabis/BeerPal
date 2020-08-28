0f74db7d1b1720d9e978e88e88115e32a0b8b153
Krzysztof <crunkz1995@gmail.com>
2020-08-28 21:29:31 +0200

    Add Endpoint generic class along with tests

---
+++ Sources/BeerPal/Services/Networking/Endpoint.swift
@@ -0,0 +1,65 @@
+//
+//  Endpoint.swift
+//  BeerPal
+//
+//  Created by Krzysztof Babis on 28.08.2020 r..
+//  Copyright © 2020 Krzysztof Babis. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+typealias Parameters = [String: Any]
+typealias Path = String
+
+final class Endpoint<Response> {
+    let method: HTTPMethod
+    let url: URL
+    let parameters: Parameters?
+    let encoding: ParameterEncoding
+    let decode: (Data) throws -> Response
+    let headers: HTTPHeaders
+
+    init(method: HTTPMethod = .get,
+         url: URL,
+         parameters: Parameters? = nil,
+         encoding: ParameterEncoding = JSONEncoding.default,
+         headers: HTTPHeaders = SessionManager.defaultHTTPHeaders,
+         decode: @escaping (Data) throws -> Response) {
+        self.method = method
+        self.url = url
+        self.parameters = parameters
+        self.encoding = encoding
+        self.headers = headers
+        self.decode = decode
+    }
+}
+
+extension Endpoint where Response: Decodable {
+    convenience init(method: HTTPMethod = .get,
+                     url: URL,
+                     parameters: Parameters? = nil,
+                     encoding: ParameterEncoding = JSONEncoding.default,
+                     headers: HTTPHeaders = SessionManager.defaultHTTPHeaders) {
+        self.init(method: method, url: url, parameters: parameters, encoding: encoding) {
+            try JSONDecoder().decode(Response.self, from: $0)
+        }
+    }
+}
+
+extension Endpoint where Response == Void {
+    convenience init(method: HTTPMethod = .get,
+                     url: URL,
+                     parameters: Parameters? = nil,
+                     encoding: ParameterEncoding = JSONEncoding.default,
+                     headers: HTTPHeaders = SessionManager.defaultHTTPHeaders) {
+        self.init(
+            method: method,
+            url: url,
+            parameters: parameters,
+            encoding: encoding,
+            headers: headers,
+            decode: { _ in () }
+        )
+    }
+}
