cfa96d1db0f4aa436fe94842ab766d70d3ea206d
Krzysztof <crunkz1995@gmail.com>
2020-08-30 20:52:41 +0200

    Add AppCoordinator along with its dependencies

---
+++ Sources/BeerPal/App/Coordinators/App/AppDependencies.swift
@@ -0,0 +1,13 @@
+//
+//  AppDependencies.swift
+//  BeerPal
+//
+//  Created by Krzysztof Babis on 30.08.2020 r..
+//  Copyright © 2020 Krzysztof Babis. All rights reserved.
+//
+
+import Foundation
+
+struct AppDependencies {
+    
+}
