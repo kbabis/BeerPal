e9c77d26bb41a0f60bc811e21184deb3de1f785c
Krzysztof <crunkz1995@gmail.com>
2020-09-01 23:29:19 +0200

    Make protocols for state views

---
+++ Sources/BeerPal/Components/Architecture/State/Protocols/DataReloading.swift
@@ -0,0 +1,13 @@
+//
+//  DataReloading.swift
+//  BeerPal
+//
+//  Created by Krzysztof Babis on 01.09.2020 r..
+//  Copyright © 2020 Krzysztof Babis. All rights reserved.
+//
+
+import Foundation
+
+protocol DataReloading {
+    func reloadData()
+}
---
+++ Sources/BeerPal/Components/Architecture/State/Protocols/StateManaging.swift
@@ -0,0 +1,13 @@
+//
+//  StateManaging.swift
+//  BeerPal
+//
+//  Created by Krzysztof Babis on 01.09.2020 r..
+//  Copyright © 2020 Krzysztof Babis. All rights reserved.
+//
+
+import Foundation
+
+protocol StateManaging {
+    var stateManager: DataStateManager { get }
+}
