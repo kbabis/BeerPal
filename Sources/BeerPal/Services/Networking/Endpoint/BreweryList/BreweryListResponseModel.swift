f55a4d7f2d3ae7a688cebe80f951807634fc6491
Krzysztof <crunkz1995@gmail.com>
2020-09-11 23:52:09 +0200

    Add response model for brewery list

---
+++ Sources/BeerPal/Services/Networking/Endpoint/BreweryList/BreweryListResponseModel.swift
@@ -0,0 +1,44 @@
+//
+//  BreweryListResponseModel.swift
+//  BeerPal
+//
+//  Created by Krzysztof Babis on 11.09.2020 r..
+//  Copyright © 2020 Krzysztof Babis. All rights reserved.
+//
+
+import Foundation
+
+struct BreweryListResponseModel: Codable {
+    let currentPage: Int
+    let numberOfPages: Int
+    let totalResults: Int
+    let breweries: [Brewery]
+    let status: String
+}
+
+struct Brewery: Codable {
+    let id, name, nameShortDisplay: String
+    let description: String?
+    let website: String?
+    let established: String?
+    let images: Images?
+    let status: Status
+    let statusDisplay: String
+    let createDate: Date
+    let updateDate: Date
+    let mailingListUrl: String?
+    @DecodableBool var isOrganic: Bool
+    @DecodableBool var isMassOwned: Bool
+    @DecodableBool var isInBusiness: Bool
+    @DecodableBool var isVerified: Bool
+    
+    struct Images: Codable {
+        let icon, medium, large: String
+        let squareMedium, squareLarge: String
+    }
+    
+    enum Status: String, Codable {
+        case unverified = "new_unverified"
+        case verified = "verified"
+    }
+}
