8af20c2e7643772d49f2e4027414c58b125ae384
Krzysztof <crunkz1995@gmail.com>
2020-09-22 00:46:01 +0200

    Set new endpoint for a beer list

--- Sources/BeerPal/Services/Networking/Endpoint/BeerList/BeerListEndpoint.swift
+++ Sources/BeerPal/Services/Networking/Endpoint/BeerList/BeerListEndpoint.swift
@@ -9,11 +9,15 @@
 import Foundation
 
 extension API {
-    static func beerList() -> Endpoint<BeerListResponseModel> {
-        let url = URLBuilder()
-        .set(path: "beers")
-        .build()!
-        
-        return Endpoint(method: .get, url: url)
+    static func beerList(name: String?, at page: Int) -> Endpoint<[Beer]> {
+        var builder = URLBuilder(scheme: "https", host: "api.punkapi.com", version: "v2")
+                        .set(path: "beers")
+                        .addQueryItem(name: "page", value: String(page))
+            
+        if let name = name {
+            builder = builder.addQueryItem(name: "beer_name", value: name)
+        }
+            
+        return Endpoint(method: .get, url: builder.build()!)
     }
 }
