55acdb556628cf323ee0082d0b099e30060f0de7
Krzysztof <crunkz1995@gmail.com>
2020-10-25 17:35:46 +0100

    Put BrewingSteps lower in the project hierarchy

---
+++ Sources/BeerPal/Scenes/BeerRecipe/Former/BrewingSteps/View/BrewingStepTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  BrewingStepTableViewCell.swift
+//  BeerPal
+//
+//  Created by Krzysztof Babis on 15.10.2020 r..
+//  Copyright © 2020 Krzysztof Babis. All rights reserved.
+//
+
+import Foundation
