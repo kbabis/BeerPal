7c483103065f7cfdc537e111cf673f7a78161321
Krzysztof <crunkz1995@gmail.com>
2020-10-31 21:49:21 +0100

    Make Double and Int value extensions public

---
+++ Sources/BeerPal/Extensions/Int+StringValue.swift
@@ -0,0 +1,15 @@
+//
+//  Int+StringValue.swift
+//  BeerPal
+//
+//  Created by Krzysztof Babis on 31.10.2020 r..
+//  Copyright © 2020 Krzysztof Babis. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+    var stringValue: String {
+        return String(self)
+    }
+}
